4b0264c1991c51f4ae37fd3f2f13bc7cfe74f6e6
Vamshi039 <vamshikittu039@gmail.com>
2022-07-06 19:34:32 +0530

    first commit from source tree

--- src/main/java/com/javatpoint/springbootcrudoperation/dto/BookDto.java
+++ src/main/java/com/javatpoint/springbootcrudoperation/dto/BookDto.java
@@ -2,4 +2,41 @@ package com.javatpoint.springbootcrudoperation.dto;
 
 public class BookDto {
 
+	private int id;
+	private String empName;
+	private String address;
+	private int number;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getEmpName() {
+		return empName;
+	}
+
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public int getNumber() {
+		return number;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
+
 }
